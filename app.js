f107aa7b61d7f497fb7d4149ff71fc41386b4112
Shivam <official.shivam0103@gmail.com>
2025-07-03 21:42:30 +0530

    Making Filter Option Dynamic

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 let imgsection = document.querySelector(".imageSection");
 // let category = document.querySelector(".category")
-console.log("hello");
+let allorien = document.querySelector("#allOrientation");
+let allcatagory = document.querySelector("#AllCategories");
+let allcolor = document.querySelector("#allcolors");
+// console.log(orien.value);
+let color;
+let orien;
+let catagory;
 
-const accessKey = "g6-LtLstaynKkYjcC26MFdzmjKPb-p8FKhi51QTzeMM";
+const accessKey = "6Jxn3JaAafmcwc0eTAj5aVvHAnfzPmL9onVcC2_Fv9E";
 
 const imagesData = async (
   query = "",
@@ -11,34 +17,51 @@ const imagesData = async (
   orientation = "",
   color = ""
 ) => {
-  // const endpoint = `https://api.unsplash.com/search/photos`;
-  // const url = new URL(endpoint);
+  const endpoint = `https://api.unsplash.com/search/photos`;
+  const url = new URL(endpoint);
 
   // Add query params dynamically
-  // url.searchParams.set('client_id', accessKey);
-  // url.searchParams.set('query', query || 'nature');  // default search query
-  // url.searchParams.set('page', page);
-  // url.searchParams.set('per_page', per_page);
-  // if (orientation) url.searchParams.set('orientation', orientation); // e.g., 'landscape'
-  // if (color) url.searchParams.set('color', color); // e.g., 'black_and_white'
+  url.searchParams.set("client_id", accessKey);
+  url.searchParams.set("query", query || "nature"); // default search query
+  url.searchParams.set("page", page);
+  url.searchParams.set("per_page", per_page);
+  if (orientation) url.searchParams.set("orientation", orientation); // e.g., 'landscape'
+  if (color) url.searchParams.set("color", color); // e.g., 'black_and_white'
 
   try {
-    // const res = await fetch(url.toString());
-    // const res = await fetch(`https://api.unsplash.com/search/photos?client_id=${accessKey}`);
-    const res = await fetch(
-      `https://api.unsplash.com/photos?client_id=${accessKey}`
-    );
+    const res = await fetch(url.toString());
     const data = await res.json();
     console.log(data);
-    return data;
+    return data.results;
   } catch (error) {
     console.error("Error fetching visual images data:", error);
   }
 };
 
+allorien.addEventListener("click", (e) => {
+  if (e.target.value.length) {
+    orien = e.target.value;
+    main();
+  }
+});
+
+allcatagory.addEventListener("click", (e) => {
+  if (e.target.value.length) {
+    catagory = e.target.value;
+    main();
+  }
+});
+
+allcolor.addEventListener("click", (e) => {
+  if (e.target.value.length) {
+    color = e.target.value;
+    main();
+  }
+});
+
 // Example usage
 let main = async () => {
-  const data = await imagesData("mountains", 1, 12, "landscape", "blue");
+  const data = await imagesData(catagory, 1, 12, orien, color);
 
   data.forEach((element) => {
     // console.log(element);
@@ -62,9 +85,8 @@ let main = async () => {
     imgdiv.appendChild(p2);
     imgdiv.appendChild(overlay);
 
-    imgsection.appendChild(imgdiv);
+    imgsection.prepend(imgdiv);
   });
 };
-
 
 main();
